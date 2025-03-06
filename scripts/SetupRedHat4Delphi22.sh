690f36ee31bafc7f895b33d3ddafee8bc82b8219
Jim McKeeth <jim@mckeeth.org>
2025-03-06 15:47:21 -0700

    12.3

--- scripts/SetupRedHat4Delphi22.sh
+++ scripts/SetupRedHat4Delphi22.sh
@@ -9,7 +9,7 @@ echo "installing development tools"
 sudo yum groupinstall 'Development Tools' -y
 sudo yum install wget gtk3 mesa-libGL gtk3-devel -y
 cd ~
-echo "Downloading Linux PAServer for Alexandria 11.3 (22.0)"
+echo "Downloading Linux PAServer for Alexandria 12.3 (23.0)"
 # wget https://altd.embarcadero.com/releases/studio/22.0/113/LinuxPAServer22.0.tar.gz # 11.3
 # wget https://altd.embarcadero.com/releases/studio/23.0/120/LinuxPAServer23.0.tar.gz # 12.0
 # wget https://altd.embarcadero.com/releases/studio/23.0/121/LinuxPAServer23.0.tar.gz # 12.1
